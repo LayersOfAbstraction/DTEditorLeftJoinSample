e06243a871ce94fdcd73f44897fbfb17d3e5898a
LayersOfAbstraction <jnash51@live.com>
2021-12-05 21:41:11 +1000

    speed up development with inserting files

---
+++ Controllers/RecipeIngredientsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DTEditorLeftJoinSample.Data;
+using DTEditorLeftJoinSample.Models;
+
+namespace DTEditorLeftJoinSample.Controllers
+{
+    public class RecipeIngredientsController : Controller
+    {
+        private readonly CookingContext _context;
+
+        public RecipeIngredientsController(CookingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RecipeIngredients
+        public async Task<IActionResult> Index()
+        {
+            var cookingContext = _context.RecipeIngredient.Include(r => r.Ingredient).Include(r => r.Recipe);
+            return View(await cookingContext.ToListAsync());
+        }
+
+        // GET: RecipeIngredients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeIngredient = await _context.RecipeIngredient
+                .Include(r => r.Ingredient)
+                .Include(r => r.Recipe)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Create
+        public IActionResult Create()
+        {
+            ViewData["IngredientID"] = new SelectList(_context.Ingredient, "ID", "ID");
+            ViewData["RecipeID"] = new SelectList(_context.Recipe, "ID", "ID");
+            return View();
+        }
+
+        // POST: RecipeIngredients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,RecipeID,IngredientID,Quantity")] RecipeIngredient recipeIngredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recipeIngredient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IngredientID"] = new SelectList(_context.Ingredient, "ID", "ID", recipeIngredient.IngredientID);
+            ViewData["RecipeID"] = new SelectList(_context.Recipe, "ID", "ID", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeIngredient = await _context.RecipeIngredient.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+            ViewData["IngredientID"] = new SelectList(_context.Ingredient, "ID", "ID", recipeIngredient.IngredientID);
+            ViewData["RecipeID"] = new SelectList(_context.Recipe, "ID", "ID", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // POST: RecipeIngredients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,RecipeID,IngredientID,Quantity")] RecipeIngredient recipeIngredient)
+        {
+            if (id != recipeIngredient.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipeIngredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeIngredientExists(recipeIngredient.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IngredientID"] = new SelectList(_context.Ingredient, "ID", "ID", recipeIngredient.IngredientID);
+            ViewData["RecipeID"] = new SelectList(_context.Recipe, "ID", "ID", recipeIngredient.RecipeID);
+            return View(recipeIngredient);
+        }
+
+        // GET: RecipeIngredients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeIngredient = await _context.RecipeIngredient
+                .Include(r => r.Ingredient)
+                .Include(r => r.Recipe)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeIngredient);
+        }
+
+        // POST: RecipeIngredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recipeIngredient = await _context.RecipeIngredient.FindAsync(id);
+            _context.RecipeIngredient.Remove(recipeIngredient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeIngredientExists(int id)
+        {
+            return _context.RecipeIngredient.Any(e => e.ID == id);
+        }
+    }
+}
